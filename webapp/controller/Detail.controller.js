72bf6a4b60a7823bc121389c95bf7a1e9aeb6dda
Quentin Kroiss <quentin.kroiss@hm.edu>
2024-06-12 23:01:22 +0200

    Last Commit

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -9,17 +9,6 @@ sap.ui.define([
 
     return Controller.extend("ui5.walkthrough.controller.Detail", {
         formatter: formatter, // Attach the formatter
-
-        onInit: function () {
-            // Initialize the view model only once during the controller's initialization
-            var oViewModel = new JSONModel({
-                showDetailedContent: false
-            });
-            this.getView().setModel(oViewModel, "view");
-        
-            // Select the InfoButton by default
-            this.onInfoPress();
-        },
         
 
         formatIncoterms: function (classification, location) {
@@ -54,16 +43,5 @@ sap.ui.define([
             }
         },
 
-        onInfoPress: function () {
-            var oModel = this.getView().getModel("view");
-            oModel.setProperty("/showDetailedContent", true);
-            oModel.setProperty("/showNoteContent", false);
-        },
-
-        onNotepadPress: function () {
-            var oModel = this.getView().getModel("view");
-            oModel.setProperty("/showDetailedContent", false);
-            oModel.setProperty("/showNoteContent", true);
-        }
     });
 });
