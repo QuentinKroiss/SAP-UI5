57d06defb5e4ef5c6cd1a3b2d78ad23679849463
Quentin Kroiss <quentin.kroiss@hm.edu>
2024-06-13 00:16:32 +0200

    Last Commit

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -8,8 +8,50 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.Detail", {
-        formatter: formatter, // Attach the formatter
-        
+        formatter: formatter,
+
+        onInit: function() {
+            var oModel = new JSONModel({
+                orderItemCount: 0
+            });
+            this.getView().setModel(oModel, "detailModel");
+        },
+
+        updateOrderItemCount: function() {
+            var oList = this.getView().byId("purchaseOrderTable");
+            var aOrderItems = oList.getItems();
+            var orderItemCount = aOrderItems.length;
+            this.getView().getModel("detailModel").setProperty("/orderItemCount", orderItemCount);
+        },
+
+        formatPricePerPiece: function(price) {
+            // Das Dezimaltrennzeichen ersetzen
+            price = price.replace('.', ',');
+            // Tausenderstellen mit einem Punkt trennen
+            price = price.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+            return price;
+        },        
+
+        calculateSubtotal: function(quantity, price) {
+            var subtotal = quantity * price;
+            // Den Wert auf zwei Nachkommastellen begrenzen und das Dezimaltrennzeichen ersetzen
+            subtotal = subtotal.toFixed(2).replace('.', ',');
+            // Tausenderstellen mit einem Punkt trennen
+            subtotal = subtotal.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+            return subtotal;
+        },
+
+        onNavBack: function () {
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                var oRouter = UIComponent.getRouterFor(this);
+                oRouter.navTo("StartScreenView", {}, true);
+            }
+        },
 
         formatIncoterms: function (classification, location) {
             if (classification && location) {
@@ -29,19 +71,7 @@ sap.ui.define([
                 percent = parseFloat(percent).toFixed(2).replace('.', ',');
             }
             return days + " Tage, " + percent + " %";
-        },
-
-        onNavBack: function () {
-            var oHistory = History.getInstance();
-            var sPreviousHash = oHistory.getPreviousHash();
-
-            if (sPreviousHash !== undefined) {
-                window.history.go(-1);
-            } else {
-                var oRouter = UIComponent.getRouterFor(this);
-                oRouter.navTo("StartScreenView", {}, true);
-            }
-        },
+        }
 
     });
 });
